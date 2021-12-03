0c0ec010e76d2570acd31fa943080ea98d91123a
Mishqa <icontab80@Gmail.com>
2021-12-03 22:04:10 +0300

    income.cpp - выделение памяти, проверка товара + кейс выбора месяца income.h - + класс с указателем

--- income.cpp
+++ income.cpp
@@ -1,1 +1,148 @@
 //файл income.cpp
+
+#include "Income.h"
+#include <iostream>
+#include <iomanip>
+#include "Struct.h"
+
+using namespace std;
+
+float insymm;
+
+Income::Income() //конструктор
+{
+	ourIncome = 0; //присваиваем указателю на структуру нулевое значение
+	prIdI = 0;
+}
+
+Income::~Income()  //дестуктор
+{
+	delete[] ourIncome;
+}
+
+void Income::addStructI(int amount) //выделение памят под объект структуры
+{
+	ourIncome = new income1[amount + 1]; //выделение памяти для первой структуры
+}
+
+void Income::addIncome(Product& objectPr, int amount) // Заполнение объекта структуры
+{
+
+	int mes, idtov;
+	float kolvotov;
+	float symm;
+	bool proverka = true;
+	objectPr.showP(amount);
+	cout << "Введите id товара: ";
+	cin >> idtov;
+	cout << "Введите количество товара: ";
+	cin >> kolvotov;
+	for (int i = 0; i < amount; i++)
+	{
+		if (idtov == objectPr.ourProduct1[i].id) // Существует ли такой товар
+		{
+			if (kolvotov > objectPr.ourProduct1[i].kolvo) // Хватает ли количества товаров
+			{
+				cout << "Недостаточно товара" << endl;
+				return;
+			}
+			else {
+				objectPr.ourProduct1[i].kolvo = objectPr.ourProduct1[i].kolvo - kolvotov; // Изменение количества товара
+				symm = kolvotov * objectPr.ourProduct1[i].price; // Авторасчет суммы продаваемого товара
+				cout << "Сумма дохода: " << symm << endl;
+				proverka = false;
+
+				cout << endl;
+				cout << "1)Январь" << endl;
+				cout << "2)Февраль" << endl;
+				cout << "3)Март" << endl;
+				cout << "4)Апрель" << endl;
+				cout << "5)Май" << endl;
+				cout << "6)Июнь" << endl;
+				cout << "7)Июль" << endl;
+				cout << "8)Август" << endl;
+				cout << "9)Сентябрь" << endl;
+				cout << "10)Октябрь" << endl;
+				cout << "110Ноябрь" << endl;
+				cout << "12)Декабрь" << endl;
+				cout << "Выберите месяц: ";
+				cin >> mes;
+				switch (mes)
+				{
+					{
+				case 1:
+				{
+					ourIncome[prIdI].Jan = symm + ourIncome[prIdI].Jan;
+					break;
+				}
+				case 2:
+				{
+					ourIncome[prIdI].Feb = symm + ourIncome[prIdI].Feb;
+					break;
+				}
+				case 3:
+				{
+					ourIncome[prIdI].Mar = symm + ourIncome[prIdI].Mar;
+					break;
+				}
+				case 4:
+				{
+					ourIncome[prIdI].Apr = symm + ourIncome[prIdI].Apr;
+					break;
+				}
+				case 5:
+				{
+					ourIncome[prIdI].May = symm + ourIncome[prIdI].May;
+					break;
+				}
+				case 6:
+				{
+					ourIncome[prIdI].Jun = symm + ourIncome[prIdI].Jun;
+					break;
+				}
+				case 7:
+				{
+					ourIncome[prIdI].Jul = symm + ourIncome[prIdI].Jul;
+					break;
+				}
+				case 8:
+				{
+					ourIncome[prIdI].Aug = symm + ourIncome[prIdI].Aug;
+					break;
+				}
+				case 9:
+				{
+					ourIncome[prIdI].Sep = symm + ourIncome[prIdI].Sep;
+					break;
+				}
+				case 10:
+				{
+					ourIncome[prIdI].Oct = symm + ourIncome[prIdI].Oct;
+					break;
+				}
+				case 11:
+				{
+					ourIncome[prIdI].Nov = symm + ourIncome[prIdI].Nov;
+					break;
+				}
+				case 12:
+				{
+					ourIncome[prIdI].Dec = symm + ourIncome[prIdI].Dec;
+					break;
+				}
+				system("pause");
+				default:
+					cout << "Месяц не найден" << endl;
+					break;
+
+					}
+				}
+			}
+		}
+
+		if (proverka == true)
+		{
+			cout << "id товара не найден" << endl;
+		}
+	}
+
--- income.h
+++ income.h
@@ -1,1 +1,18 @@
 // файл Income.h
+
+#ifndef __INCOME_H__
+#define __INCOME_H__
+#include "Struct.h"
+#include "Product.h"
+
+using namespace std;
+
+class income //доход
+{
+	private:
+		income1* ourIncome; // ”казатель на структуру доходы
+		int prIdI; // id объекта дохода
+
+	public:
+};
+#endif
