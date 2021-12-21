2a16b1b877df76b78db6e9d3d3d3ea1828859267
oleg <oleg20064@gmail.com>
2021-12-21 14:09:07 +0300

    Revert "Небольшие правки проекта."
    This reverts commit 367cb5d4df8dd8409ed4c3e35ea37a122de9d3aa.

--- income.h
+++ income.h
@@ -1,25 +1,23 @@
-// файл Income.h
-
 #ifndef __INCOME_H__
 #define __INCOME_H__
 #include "Struct.h"
-#include "Product.h"
+#include "product.h"
 
 using namespace std;
 
 class Income //доход
 {
-	private:
-		income1* ourIncome; // Указатель на структуру доходы
-		int prodId; // id объекта дохода
+private:
+    income1* ourIncome; // Указатель на структуру доходы
+    int prodId; // id объекта дохода
 
-	public:
-		Income();
-		~Income();
-		void addIncome(Product&, int); // Создание объекта доходы
-		void addStructIncome(int); // Выделение памяти под объект дохода
-		void showIncome(); // Вывод таблицы доходов
-		void totalIncomeSum(); // Общая сумма доходов
+public:
+    Income();
+    ~Income();
+    void addIncome(Product&, int); // Создание объекта доходы
+    void addStructIncome(int); // Выделение памяти под объект дохода
+    void showIncome(); // Вывод таблицы доходов
+    void totalIncomeSum(); // Общая сумма доходов
 
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include "Product.h"
-#include "Income.h"
+#include "product.h"
+#include "income.h"
 #include "Expenses.h"
 #include "function.h"
 #include <iostream>
@@ -10,107 +10,108 @@ using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, "rus");
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	int item;
-	Product objectPr; // Создаем объект класса Product
-	Income objectIn; // Создаем объект класса Income
-	Expenses objectEx; // Создаем объект класса Expenses
-	int productAmount = 0; // Начальное количество товаров 
-	int incomeAmount = 0; // Начальный доход
-	int expensesAmount = 0;	// Начальный расход
-	objectIn.addStructIncome(incomeAmount);
+    setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int a;
+    Product objectPr; // Создаем объект класса Product
+    Income objectIn; // Создаем объект класса Income
+    Expenses objectEx; // Создаем объект класса Expenses
+    int productAmount = 0; // Присваиваем количество товара
+    int incomeAmount = 0;
+    int expensesAmount = 0;
+    objectIn.addStructIncome(incomeAmount);
 
-	do { // Цикл меню программы
-		menu(); // Вызов функции, отвечающей выводу основного меню программы
-		cin >> item;
-		system("cls"); // Очистка экрана
+    do { // Цикл меню программы
+        menu(); // Вызов функции, отвечающей выводу основного меню программы
+        cin >> a;
+        system("cls"); // Очистка экрана
 
-		switch (item)
-		{
-		case  1: // Добавление товара
-		{
-			objectPr.addStruct(productAmount); // Вызов метода объекта класса Product
-			objectPr.addProduct(productAmount);
-			productAmount++;
-			break;
-		}
-		case  2: // Ввод расходов
-			objectEx.addStructExpences(expensesAmount);  //Вызов метода объекта класса Expenses
-			objectEx.addExpenses(expensesAmount);
-			expensesAmount++;
-			break;
-		case  3: // Ввод доходов
-			objectPr.copyProd(productAmount);
-			objectIn.addIncome(objectPr, productAmount); //Вызов метода объекта класса Income
-			objectPr.recopyProd(productAmount);
-			break;
-		case  4: // Вывод закупок
-			objectEx.addStructExpences(expensesAmount);
-			objectPr.copyProd(productAmount);
-			objectEx.purchase(objectPr, productAmount, expensesAmount);
-			objectPr.recopyProd(productAmount);
-			expensesAmount++;
-			break;
-		case  5: // Редактирование/просмотр товаров
-		{
-			int action;
-			punkt5(); // Вызов функции, отвечающей за вывод меню действия с товарами
-			cin >> action;
-			system("cls");
-			switch (action)
-			{
-			case 1:	// Вывод списка товаров
-			{
-				if (productAmount != 0) {
-					objectPr.showProd(productAmount);
-				}
-				else {
-					cout << "Нет товаров" << endl;
-				}
-				break;
-			}
-			case 2:	// Редактирование товаров
-			{
-				objectPr.editProd(productAmount);
-				break;
-			}
-			case 0:	// Выход в основное меню
-			{
-				break;
-			}
-			system("pause");
-			default: // Если неверный case
-				cout << "Такой операции не существует" << endl;
-				break;
-			}
-		}
-		break;
-		case  6: // Вывод расходов
-			objectIn.showIncome();
-			cout << endl;
-			break;
-		case  7: // Вывод доходов
-			objectEx.showExpences(expensesAmount);
-			break;
-		case  8: // Вывод годового отчета
-			objectEx.dataReport(expensesAmount);
-			objectIn.totalIncomeSum();
-			objectPr.leftoversSum(productAmount);
-			objectEx.annualReport();
-			break;
-		case  0: // Выход из программы
-			return 0;
-			break;
+        switch (a)
+        {
+            case  1:
+            {
+                objectPr.addStruct(productAmount); // Вызов метода объекта класса Product
+                objectPr.addProduct(productAmount);
+                productAmount++;
+                break;
+            }
 
-		default: // Если неверный case
-			cout << "Такой операции не существует" << endl;
-			break;
-		}
-		system("pause");
-		system("cls");
-	} while (item != 0); // Условие выхода из цикла
+            case  2:
+                objectEx.addStructExpences(expensesAmount);  //Вызов метода объекта класса Expenses
+                objectEx.addExpenses(expensesAmount);
+                expensesAmount++;
+                break;
+            case  3:
+                objectPr.copyProd(productAmount);
+                objectIn.addIncome(objectPr, productAmount); //Вызов метода объекта класса Income
+                objectPr.recopyProd(productAmount);
+                break;
+            case  4:
+                objectEx.addStructExpences(expensesAmount);
+                objectPr.copyProd(productAmount);
+                objectEx.purchase(objectPr, productAmount, expensesAmount);
+                objectPr.recopyProd(productAmount);
+                expensesAmount++;
+                break;
+            case  5:
+            {
 
-	return 0;
+                int deistvie;
+                punkt5(); // Вызов функции, отвечающей за вывод меню действия с товарами
+                cin >> deistvie;
+                system("cls");
+
+                switch (deistvie)
+                {
+                    case 1:
+                    {
+                        if (productAmount != 0) {
+                            objectPr.showProd(productAmount);
+                        }
+                        else {
+                            cout << "Нет товаров" << endl;
+                        }
+                        break;
+                    }
+                    case 2:
+                    {
+                        objectPr.editProd(productAmount);
+                        break;
+                    }
+                    case 0:
+                    {
+                        break;
+                    }
+                        system("pause");
+                    default:
+                        cout << "Такой операции не существует" << endl;
+                        break;
+                }
+            }
+                break;
+            case  6:
+                objectIn.showIncome();
+                cout << endl;
+                break;
+            case  7:
+                objectEx.showExpences(expensesAmount);
+                break;
+            case  8:
+                objectEx.dataReport(expensesAmount);
+                objectIn.totalIncomeSum();
+                objectPr.leftoversSum(productAmount);
+                objectEx.annualReport();
+                break;
+            case  0:
+                return 0;
+            default:
+                cout << "Такой операции не существует" << endl;
+                break;
+        }
+        system("pause");
+        system("cls");
+    } while (a != 0); // Условие выхода из цикла
+
+    return 0;
 }
